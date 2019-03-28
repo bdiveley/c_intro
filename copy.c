f3b95654067b834b47c9d9a7d30629a6abc0e5b1
Bailey Diveley <bailey.diveley@gmail.com>
2019-03-28 10:48:27 -0600

    Allocating memory

--- copy.c
+++ copy.c
@@ -8,14 +8,28 @@ int main(int argc, char *argv[]) {
   char *s = argv[1];
 
 // this copies the address of s into t
-  char *t = s;
+if (!s)
+{
+  return 1;
+}
+// malloc = memory allocate
+  char *t = malloc(strlen(s) + 1);
 
-  if (strlen(t) > 0)
+  if(!t)
   {
-    t[0] = toupper(t[0]);
+    return 1;
   }
 
-// prints both strings capitalized bc they both point to the same memory location 
+  for (int i = 0, n= strlen(s);i <= n; i++)
+  {
+    if (strlen(t) > 0)
+    {
+      t[0] = toupper(t[0]);
+    }
+      t[i] = s[i];
+  }
+
+// t now capitalized, s remains lowercase
   printf("%s\n", s);
   printf("%s\n", t);
 }
