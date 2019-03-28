714f3bc3a4c28b9ee150d3be0f3cf75345d55f7e
Bailey Diveley <bailey.diveley@gmail.com>
2019-03-28 11:35:20 -0600

    Print memory locations

---
+++ addresses.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <cs50.h>
+
+
+int main(void)
+{
+  char s[6];
+  printf("Input: ");
+  scanf("%s", s);
+
+  char t[6];
+  printf("Input: ");
+  scanf("%s", t);
+
+// %p will print the memroy location of s
+  printf("s: %p\n", s);
+// returns s: 0x7ffee0e86ada
+  printf("t: %p\n", t);
+// returns t: 0x7ffee0e86ad4
+
+}
