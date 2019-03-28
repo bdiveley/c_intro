f0e9486ad043137a621c7ce2c0f9d477bd39d88c
Bailey Diveley <bailey.diveley@gmail.com>
2019-03-28 10:23:16 -0600

    Add error codes for null values

--- compare1.c
+++ compare1.c
@@ -6,7 +6,17 @@ int main(void)
 {
     //get ride of string and replace with string
     char *name = "Bailey";
+    if (name == NULL)
+    {
+      // returning 1 means there's an error
+      return 1;
+    }
     char *name2 = "Diveley";
+    if (name2 == NULL)
+    {
+      // returning 1 means there's an error
+      return 1;
+    }
 
     //function that compares ASCII values
     if (strcmp(name, name2) == 0)
