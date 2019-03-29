52c97bed916fd862bea2095d9c29fa70d8188c6a
Bailey Diveley <bailey.diveley@gmail.com>
2019-03-29 13:03:56 -0600

    Dynamically change array size with new user input

---
+++ list1.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+
+  int *numbers = NULL;
+  int capacity = 0;
+
+  int size = 0;
+  while (true)
+  {
+    int number = get_int("Number: ");
+
+    if (number == INT_MAX)
+    {
+      break;
+    }
+
+    if (size == capacity)
+      {
+        numbers = realloc(numbers, sizeof(int) * (size + 1))
+      }
+
+      numbers[size] = number;
+      size++;
+      capacity++;
+  }
+
+  for (int i = 0 ; i < size; i++)
+  {
+    printf("You inputted %i\n", numbers[i]);
+  }
+}
